3a13e13d7570a679c5ba92032c47e81386d45920
Rodrigo Silveira de Santiago <rodrigosantiago838@gmail.com>
2022-08-11 21:41:42 -0300

    Project Working

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-import sys
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+import os
+from PyPDF2 import PdfMerger
+def get_files(dir):
+    merger = PdfMerger()
+    for file in os.listdir(dir):
+        print(file.split(' - ')[0])
+        merger.append(f'{dir}/{file}')
+    merger.write(f'{dir}/result.pdf')
+    merger.close()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    dir = "folder"
+    get_files(dir)
